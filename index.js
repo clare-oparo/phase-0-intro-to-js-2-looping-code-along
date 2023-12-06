57e2795a1ce63c6d5e6e52b028128837bc84a83d
Clare <clare.oparo@student.moringaschool.com>
2023-12-06 13:45:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards( gifterNames, event ) {
+    let thanksMessage = []
+    for ( let i = 0; i < gifterNames.length; i++ ) {
+      thanksMessage.push( `Thank you, ${gifterNames[i]}, for the wonderful ${event} gift!` )
+    }
+    return thanksMessage
+  }
+
+  function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
